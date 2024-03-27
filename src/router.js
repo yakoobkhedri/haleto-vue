51e6c7959793de75b96232e1c7a2c85f82c6f1ea
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-03-27 23:16:19 +0330

    commit message

--- src/router.js
+++ src/router.js
@@ -2,11 +2,13 @@ import { createRouter,createWebHistory } from "vue-router";
 import Home from './pages/Home.vue';
 import Cadr from './pages/Cadr.vue';
 import Request from './pages/Request.vue';
+import SingleBlog from './pages/SingleBlog.vue';
 
 const routes=[
     {name:'home',path:'/',component:Home},
     {name:'cadr',path:'/cadr',component:Cadr},
     {name:'request',path:'/request',component:Request},
+    {name:'singleBlog',path:'/singleBlog',component:SingleBlog},
 ];
 
 const router=createRouter({
